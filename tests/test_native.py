67c71c27302847a9e1b9cbf70c5ac6e0c754a4e2
Mohammed Nabid <82520116+mnabid05@users.noreply.github.com>
2026-08-30 16:46:07 -0400

    test(eval): remove rejected evaluation expectations

--- tests/test_native.py
+++ tests/test_native.py
@@ -24,22 +24,6 @@ class NativeEvaluatorTests(unittest.TestCase):
             -self.evaluator.evaluate(black),
         )
 
-    def test_loose_piece_is_penalized_when_attacked_by_pawn(self):
-        safe = Board.from_fen("7k/8/3p4/8/4N3/8/8/7K w - - 0 1")
-        threatened = Board.from_fen("7k/8/8/3p4/4N3/8/8/7K w - - 0 1")
-        self.assertLess(
-            self.evaluator.evaluate(threatened),
-            self.evaluator.evaluate(safe),
-        )
-
-    def test_nearby_enemy_pawn_increases_king_danger(self):
-        distant = Board.from_fen("7k/8/8/8/p7/8/8/6K1 w - - 0 1")
-        pressure = Board.from_fen("7k/8/8/8/6p1/8/8/6K1 w - - 0 1")
-        self.assertLess(
-            self.evaluator.evaluate(pressure),
-            self.evaluator.evaluate(distant),
-        )
-
 
 if __name__ == "__main__":
     unittest.main()
